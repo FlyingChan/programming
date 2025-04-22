f474f811f5f4392ddb4d48804cb9086146b6c0da
WoochanKang <woosan0525@gmail.com>
2025-04-22 22:06:44 +0900

    first

---
+++ Week1-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("*%8d*\n",1234);
+    printf("*%-8d*\n",1234);
+    printf("*%.1f\n",1.234);
+    printf("*%8.1f*\n",1.234);
+    printf("*%-8.1f*\n",1.234);    
+}
